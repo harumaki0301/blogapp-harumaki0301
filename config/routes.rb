87e7d36ce994633555b9ee746e049c2c45ddd9aa
makiko <abcmakiko@gmail.com>
2020-08-02 15:53:28 +0900

    articles controllerに変更

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
 Rails.application.routes.draw do
-  root to: 'home#index'
+  root to: 'articles#index'
   
-  get 'about' => 'home#about'
 end
