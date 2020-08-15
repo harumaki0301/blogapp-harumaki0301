5483a3748358bd52ed739b3ac4fdae4a26d1d54a
makiko <abcmakiko@gmail.com>
2020-08-15 14:27:04 +0900

    deviseの導入

--- app/models/article.rb
+++ app/models/article.rb
@@ -12,14 +12,12 @@ class Article < ApplicationRecord
     validates :title, presence:true
     validates :title, length: { minimum: 2, maximum: 100 }
     validates :title, format: { with: /\A(?!\@)/}
-    
+
     validates :content, presence:true
     validates :content, length: { minimum: 10 }
     validates :content, uniqueness: true
 
     validate :validate_title_and_content_length
-
-
 
     def display_created_at
         I18n.l(self.created_at, format: :default)
